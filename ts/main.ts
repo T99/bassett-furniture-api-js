8848b3346a04e1a73c69f94d88c04f6c59ab6ce2
Trevor Sears <trevorsears.main@gmail.com>
2021-11-11 11:28:01 -0500

    Updated version template in `main.ts`.

--- ts/main.ts
+++ ts/main.ts
@@ -8,8 +8,8 @@
  * NPM main class used for exporting this package's contents.
  *
  * @author Trevor Sears <trevorsears.main@gmail.com>
- * @version v<version>
- * @since v<version>
+ * @version v0.1.0
+ * @since v0.1.0
  */
 
 // export { ClassName } from "./class-location";
